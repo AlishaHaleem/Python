ac6a6c6a28668140d71c91379d5663af5c6b0cad
AlishaHaleem <halisha071@gmail.com>
2024-11-24 19:00:05 +0500

    update

--- Day_1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the 100 days of python course")
-name = input("What is your course name?/n")
-website = input("From which academy you are learning./n")
-print(f"Your course name is {name} and your academy is {website}.")
