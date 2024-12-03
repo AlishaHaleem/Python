7ae3eacc2ed3ab8d64515ec301dcbee02beae536
AlishaHaleem <halisha071@gmail.com>
2024-12-03 09:35:06 +0500

    update

---
+++ Day 8 part_2.py
@@ -0,0 +1,29 @@
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+directions = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_text,shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter) + shift_amount
+        position %= len(alphabet)
+        cipher_text += alphabet[position]
+    print(f"The encoded text is {cipher_text}")
+
+# TODO: Create a function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text,shift_amount):
+    output_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter) - shift_amount
+        position %= len(alphabet)
+        output_text += alphabet[position]
+    print(f"The decoded text is {output_text}")
+
+
+#TODO: Inside the 'encrypt' function, shift each letter of the 'text'
+# forwards in the alphabet by the shift amount and print the encrypted text.
+encrypt(plain_text=text,shift_amount=shift)
+decrypt(plain_text=text,shift_amount=shift)
