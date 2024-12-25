0b3a026e2aa32e68c460d6f5e889a4bf1ffce8e4
AlishaHaleem <halisha071@gmail.com>
2024-12-25 12:00:07 +0500

    update

--- Day_21/scoreboard.py
+++ Day_21/scoreboard.py
@@ -1,7 +1,28 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "center"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
 
-# TODO: Create a Scoreboard class
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.goto(-200, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+
