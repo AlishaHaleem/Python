c242d59cdb1a152335b361df1fc7b4bed803f6d3
AlishaHaleem <halisha071@gmail.com>
2025-01-08 10:43:18 +0500

    update

--- Day_27/main.py
+++ Day_27/main.py
@@ -1,1 +1,28 @@
 print("aws")
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.grid(column=0, row=0)
+
+
+import turtle as t
+tim = t.Turtle()
+tim.write("Some text")
+tim.shape("turtle")
+tim.color("red")
+tim.forward(100)
+
+
+
+
+
+
+
+
+window.mainloop()
