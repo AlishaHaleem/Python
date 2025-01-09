92863d3e717b936f095be471e2d95ceeaa03540a
AlishaHaleem <halisha071@gmail.com>
2025-01-09 17:02:10 +0500

    update

--- Day_28/main.py
+++ Day_28/main.py
@@ -1,1 +1,21 @@
 print("Default values")
+print("Hello world")
+
+print("Positional arguments")
+
+
+def add(n1 , n2):
+    print(n1 + n2)
+
+
+add(5, 10)
+
+
+print("Keyword arguments")
+
+
+def add(n1 , n2):    # n1 = 5 , n2 = 10
+    print(n1 + n2)    # 5 + 10 = 15
+
+
+add(n2=10, n1=5)
